c62bb94ca9f33b45ca3e07bedc4577d4938fc83d
the02196 <the02196@gmail.com>
2023-11-20 13:48:11 +0900

    db 수정

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-import db from 'mysql2/promise'
-
-const connectDB = db.createPool ({
-    host: `${process.env.NEXT_PUBLIC_DATABASE_HOST}`,
-    user: `${process.env.NEXT_PUBLIC_DATABASE_USERNAME}`,
-    password: `${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}`,
-    database: `${process.env.NEXT_PUBLIC_DATABASE_NAME}`,
-    ssl : {
-        rejectUnauthorized: true
-    }
-})
-
-export default connectDB
+import db from "mysql2/promise";
+const connectDB = db.createPool({
+  host: `${process.env.NEXT_PUBLIC_DATABASE_HOST}`,
+  user: `${process.env.NEXT_PUBLIC_DATABASE_USERNAME}`,
+  password: `${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}`,
+  database: `${process.env.NEXT_PUBLIC_DATABASE_NAME}`,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
+export default connectDB;
