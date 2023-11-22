84e8f265ff37b32e49ba950f4f0b29519f105068
the02196 <the02196@gmail.com>
2023-11-22 09:52:18 +0900

    배포 에러로 admin 일시적으로 주석 처리

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,30 +1,30 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import ChartCom from "./chart";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import ChartCom from "./chart";
 
-interface userInfo {
-  user: {
-    name: string;
-    email?: string;
-    image?: string;
-    level?: number;
-  };
-}
+// interface userInfo {
+//   user: {
+//     name: string;
+//     email?: string;
+//     image?: string;
+//     level?: number;
+//   };
+// }
 
-export default async function Admin() {
+// export default async function Admin() {
  
-  let sessions = (await getServerSession(authOptions)) as userInfo;
-  // nextjs는 폴더 이름 자체가 라우터 이기 때문에, 여기서 바로 조건문을 걸면
-  // 라우터에 들어오는 즉시 유효성 검사를 한다.
+//   let sessions = (await getServerSession(authOptions)) as userInfo;
+//   // nextjs는 폴더 이름 자체가 라우터 이기 때문에, 여기서 바로 조건문을 걸면
+//   // 라우터에 들어오는 즉시 유효성 검사를 한다.
 
-  if ((!sessions && sessions) || sessions?.user.level !== 10) {
-    return <p>관리자만 접속 가능한 페이지입니다.</p>;
-  }
+//   if ((!sessions && sessions) || sessions?.user.level !== 10) {
+//     return <p>관리자만 접속 가능한 페이지입니다.</p>;
+//   }
 
-  return (
-    <>
-      <p>관리자 전용</p>
-      <ChartCom />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <p>관리자 전용</p>
+//       <ChartCom />
+//     </>
+//   );
+// }
